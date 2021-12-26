3e5ebbfe07c801379aa7ca7b0ff0c6dc300a3419
bong-u <bongudev@gmail.com>
2021-12-26 07:11:31 +0000

    new build

--- src/project.js
+++ src/project.js
@@ -7,28 +7,28 @@ class Project extends React.Component {
 		return (
 			<div id="projects">
 				<div>
-					<a className = "item" href="https://bong-u.github.io/bong-u/game/tetris">
-						<img src="/tetris.png" alt=""></img>
+					<a className = "item" href="https://bong-u.github.io/game/tetris">
+						<img src="tetris.png" alt=""></img>
 						<span>Tetris</span>
 					</a>
 				</div>
 				<div>
-					<a className = "item" href="https://bong-u.github.io/bong-u/game/minesweeper">
-						<img src="/minesweeper.png" alt=""></img>
+					<a className = "item" href="https://bong-u.github.io/game/minesweeper">
+						<img src="minesweeper.png" alt=""></img>
 						<span>MineSweeper</span>
 					</a>
 				</div>
 				
 				<div>
-					<a className = "item" href="https://bong-u.github.io/bong-u/game/worm">
-						<img src="/tetris.png" alt=""></img>
+					<a className = "item" href="https://bong-u.github.io/game/worm">
+						<img src="tetris.png" alt=""></img>
 						<span>Worm</span>
 					</a>
 				</div>
 				
 				<div>
-					<a className = "item" href="https://bong-u.github.io/bong-u/game/tetris">
-						<img src="/minesweeper.png" alt=""></img>
+					<a className = "item" href="https://bong-u.github.io/game/tetris">
+						<img src="minesweeper.png" alt=""></img>
 						<span>Bartist</span>
 					</a>
 				</div>
